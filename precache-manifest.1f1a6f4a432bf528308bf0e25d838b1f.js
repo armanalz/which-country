3ef7985d11abf2c61e9bc0945f0eb125906add51
Arman Alizadeh <armanalizadeh467@gmail.com>
2020-06-06 17:39:41 +0200

    Updates

---
+++ precache-manifest.1f1a6f4a432bf528308bf0e25d838b1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5127abc39690603b15ea0ef829ba6cf4",
+    "url": "/which-country/index.html"
+  },
+  {
+    "revision": "05212ae9608f0675bc14",
+    "url": "/which-country/static/css/main.66663f07.chunk.css"
+  },
+  {
+    "revision": "6994d06984cd076700c2",
+    "url": "/which-country/static/js/2.3258c80b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/which-country/static/js/2.3258c80b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05212ae9608f0675bc14",
+    "url": "/which-country/static/js/main.af2b6f09.chunk.js"
+  },
+  {
+    "revision": "ded9e564df623098728e",
+    "url": "/which-country/static/js/runtime-main.a7b0bd2b.js"
+  },
+  {
+    "revision": "6e7bab33414804b7dbf34e08874afc7b",
+    "url": "/which-country/static/media/sprite.6e7bab33.svg"
+  }
+]);
